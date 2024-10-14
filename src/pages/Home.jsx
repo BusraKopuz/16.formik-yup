bfc4beab98dc27a9641d65764af7a0cf09f0d0c8
BusraKopuz <kopuzbusra16@gmail.com>
2024-10-14 19:27:46 +0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import RegisterForm from '../components/RegisterForm';
+
+import '../App.css'
+
+
+function Home() {
+ 
+    return (
+        <div className='App'>
+            <RegisterForm />    
+        </div>
+    )
+}
+
+export default Home
